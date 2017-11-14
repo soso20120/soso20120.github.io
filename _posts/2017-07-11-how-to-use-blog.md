c1d362904ace9387b4c5860aa2068bbe91eb8c3d
meiLishi <597705992@qq.com>
2017-11-14 10:26:24 +0800

    tab增加

---
+++ _posts/2017-07-11-how-to-use-blog.md
@@ -0,0 +1,17 @@
+---
+layout: post 
+title: "BLOG怎么用-Jekyll目录"
+date: 2017-07-11 11:11:11 +0800
+categories: [Blog]
+excerpt: 这是一个学习blog的经验过程。不断更新。
+tags:
+  - jekyll
+---
+
+
+<!-- * content
+{:toc} -->
+
+
+附上搭博客的指导：
+<a href="http://www.jianshu.com/p/8f843034c7ec">“授人以渔”的教你搭建个人独立博客</a>
--- _site/assets/js/script.js
+++ _site/assets/js/script.js
@@ -21,22 +21,30 @@ $(document).ready(function () {
 	if (tabContainer.length) {
 		$(".tab-two").bind("click", showTabTwo);
 		$(".tab-one").bind("click", showTabOne);
+    $(".tab-three").bind("click", showTabThree);
 	}
 	function showTabOne () {
 		$(".tab-one").addClass("active");
 		$(".tab-two").removeClass("active");
+    $(".tab-three").removeClass("active")
 		$(".tab-two-list").addClass("tab-hidden");
+    $(".tab-three-list").addClass("tab-hidden");
 		$(".tab-one-list").removeClass("tab-hidden");
 		$(".page-holder-two").addClass("tab-hidden");
+    $(".page-holder-three").addClass("tab-hidden");
 		$(".page-holder-one").removeClass("tab-hidden");
 	}
 	function showTabTwo () {
 		$(".tab-two").addClass("active");
 		$(".tab-one").removeClass("active");
+    $(".tab-three").removeClass("active");
 		$(".tab-one-list").addClass("tab-hidden");
+    $(".tab-three-list").addClass("tab-hidden");
 		$(".tab-two-list").removeClass("tab-hidden");
 		$(".page-holder-one").addClass("tab-hidden");
+    $(".page-holder-three").addClass("tab-hidden");
 		$(".page-holder-two").removeClass("tab-hidden");
+
 	}
 })
 
@@ -58,6 +66,17 @@ $(function(){
   });
   $("div.page-holder-two").jPages({
       containerID  : "pag-itemContainer-two",
+      previous: "«",
+      next: "»",
+      perPage      : 5,  /* num of items per page */
+      startPage    : 1,
+      startRange   : 1,
+      midRange     : 4,
+      endRange     : 1,
+      direction    : "auto"
+  });
+  $("div.page-holder-three").jPages({
+      containerID  : "pag-itemContainer-three",
       previous: "«",
       next: "»",
       perPage      : 5,  /* num of items per page */
